07c6895cd0aa2cde8eedb54ef70c9e469412f3dc
shuyangdai <dsy115@gmail.com>
2017-06-20 20:30:49 -0700

    go

--- js/script.js
+++ js/script.js
@@ -168,7 +168,7 @@ $(document).ready(function(){
 	setTimeout( function(){
 		if($("#site-type").length) {
         $(".typewrite span").typed({
-            strings: ["show case ", "projects "],
+            strings: ["Design ", "Illustration ","Storytelling"],
             typeSpeed: 100,
             backDelay: 500,
             loop: false,
